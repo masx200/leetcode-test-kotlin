4c1bbe03ee4e8521eeec4e0c41195012428ae4cb
masx200 <34191203+masx200@users.noreply.github.com>
2024-04-27 23:13:05 +0800

    Update index.kt

--- src/total-distance-traveled/index.kt
+++ src/total-distance-traveled/index.kt
@@ -1,7 +1,9 @@
 package com.github.masx200.leetcode_test_kotlin.total_distance_traveled
 
+import kotlin.math.min
+
 class Solution {
     fun distanceTraveled(mainTank: Int, additionalTank: Int): Int {
-        return 10 * (mainTank + min((mainTank - 1) / 4, additionalTank));
+        return 10 * (mainTank + min((mainTank - 1) / 4, additionalTank))
     }
 }
