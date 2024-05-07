366297e6dac2e800249ec9504670afb65e92614f
masx200 <34191203+masx200@users.noreply.github.com>
2024-05-07 14:17:11 +0800

    jsonArraytoArrayIntArray

--- test/cherry_pickup_ii/SolutionTest.kt
+++ test/cherry_pickup_ii/SolutionTest.kt
@@ -31,17 +31,18 @@ class SolutionTest {
         assertEquals(28, solution.cherryPickup(grid))
     }
 
-    fun jsonArraytoArrayIntArray(values: JSONArray): Array<IntArray> {
-        val result = Array<IntArray>(values.size, { IntArray(0) })
-        for (i in 0 until values.size) {
-            val value = values.getJSONArray(i)
-            val intArray = IntArray(value.size)
-            for (j in 0 until value.size) {
-                intArray[j] = value.getIntValue(j)
-            }
-            result[i] = (intArray)
-        }
-        return result
-    }
 
+}
+
+fun jsonArraytoArrayIntArray(values: JSONArray): Array<IntArray> {
+    val result = Array<IntArray>(values.size, { IntArray(0) })
+    for (i in 0 until values.size) {
+        val value = values.getJSONArray(i)
+        val intArray = IntArray(value.size)
+        for (j in 0 until value.size) {
+            intArray[j] = value.getIntValue(j)
+        }
+        result[i] = (intArray)
+    }
+    return result
 }
